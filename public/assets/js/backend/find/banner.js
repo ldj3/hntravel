0737a2f7b2eeaa19f5cba4eca152f2eace78183c
ldj3 <1437368102@qq.com>
2018-05-20 23:57:19 +0800

    首页

---
+++ public/assets/js/backend/find/banner.js
@@ -0,0 +1,67 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function() {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'find/banner/index',
+                    add_url: 'find/banner/add',
+                    edit_url: 'find/banner/edit',
+                    del_url: 'find/banner/del',
+                    table: 'banner',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                escape: false,
+                pk: 'id',
+                sortName: 'id',
+                pagination: false,
+                commonSearch: false,
+                columns: [
+                    [
+                        { checkbox: true },
+                        { field: 'id', title: __('Id') },
+                        {
+                            field: 'image',
+                            title: __('图片'),
+                            formatter: Table.api.formatter.image,
+                            operate: false
+                        },
+                        {
+                            field: 'place',
+                            title: __('轮播图')
+                        },
+                        { field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate }
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function() {
+            Controller.api.bindevent();
+        },
+        edit: function() {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function() {
+                $(document).on("change", "#c-type", function() {
+                    $("#c-pid option[data-type='all']").prop("selected", true);
+                    $("#c-pid option").removeClass("hide");
+                    $("#c-pid option[data-type!='" + $(this).val() + "'][data-type!='all']").addClass("hide");
+                    $("#c-pid").selectpicker("refresh");
+                });
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
